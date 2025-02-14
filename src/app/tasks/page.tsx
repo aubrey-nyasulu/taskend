d48fc8c75b32baa50c315e84b3b69b0f8242395c
Obob <aubreynyasulu98@gmail.com>
2025-02-14 20:24:33 +0200

    initial commit

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex-1 flex flex-col items-center gap-10 min-h-screen px-4">
+      <div className="w-full h-16 rounded-2xl mt-4"></div>
+
+      <section className="bg-dark-bg w-full flex-1 rounded-t-[32px]"></section>
+    </main>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-bg": "#363636",
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
